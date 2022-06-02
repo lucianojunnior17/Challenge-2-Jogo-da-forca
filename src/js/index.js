4526c67bad4394f0c1a3abeca4c67adae794a3df
lucianojunnior17 <engluciano.junior@gmail.com>
2022-06-02 18:28:23 -0300

    tracinho

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,33 @@
-var tela = document.querySelector('canvas');
-var pincel = tela.getContext('2d');
-    //retangulo
-pincel.fillStyle='blue';
-pincel.fillRect(0, 0,1280, 900);
-    
+//VARIAVÉIS 
+
+
+var palavras = ['ALURA','ORACLE','ABACAXI','MORANGO','EMPREGO'];
+var tela = document.getElementById('forca');
+var tabuleiro =tela.getContext('2d');
+var letras = [];
+var palavraCorreta = '';
+var erros = 9;
+
+function EcolherPalavraSecreta(){
+    var palavra = palavras[Math.floor(Math.random()*palavras.length)];
+    Secreta = palavra;
+    console.log(palavra);
+    return palavra;
+}
+
+function escreverTracinho(){
+    tabuleiro.lineWidth = 6
+    tabuleiro.lineCap = "round"
+    tabuleiro.lineJoin = "round"
+    tabuleiro.strokeStyle = "#000000"
+    tabuleiro.beginPath()
+    var eixo = 600/Secreta.length
+    for(let i = 0; i < Secreta.length; i++){
+        tabuleiro.moveTo(600 + (eixo*i),640);
+        tabuleiro.lineTo(550 + (eixo*i),640);
+    }
+    tabuleiro.stroke()
+    tabuleiro.closePath()
+}escreverTracinho(EcolherPalavraSecreta());
+
+
