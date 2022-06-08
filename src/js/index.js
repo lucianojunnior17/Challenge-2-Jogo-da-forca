3f281e9e93dc58a52849acd4ba60605ecb4ff7af
lucianojunnior17 <engluciano.junior@gmail.com>
2022-06-08 18:03:50 -0300

    palavra-errada

--- src/js/index.js
+++ src/js/index.js
@@ -30,4 +30,68 @@ function escreverTracinho(){
     tabuleiro.closePath()
 }escreverTracinho(EcolherPalavraSecreta());
 
+function escreverLetraCorreta(index){
+    tabuleiro.font = 'bold 52px Koulen';
+    tabuleiro.lineWidth = 6
+    tabuleiro.lineCap = "round"
+    tabuleiro.lineJoin = "round"
+    tabuleiro.strokeStyle = "#000000"
 
+    var eixo =  600/Secreta.length
+    tabuleiro.fillText(Secreta[index],560+(eixo*index), 620)
+    tabuleiro.stroke()
+}
+
+function ecreverLetraIncorreta(letras, errosLeft){
+    tabuleiro.font = 'bold 40px Koulen';
+    tabuleiro.lineWidth = 6
+    tabuleiro.lineCap = "round"
+    tabuleiro.lineJoin = "round"
+    tabuleiro.strokeStyle = "#000000"
+    tabuleiro.fillText(letras, 535+(40*(10-errosLeft)),710,40)
+}
+
+function verificarLetraCorreta(key){
+    if(letras.length < 1 || letras.indexOf(key) < 0 ){
+        console.log(key)
+        letras.push(key)
+        return false
+    }
+    else{
+        letras.push(key.toUpperCase())
+        return true
+    }
+}
+
+function adicinarLetraCorreta (i){
+    palavraCorreta += Secreta[i].toUpperCase()
+}
+
+function adicionarLetraIncorreta (letter) {
+    if(Secreta.indexOf(letter) <= 0){
+        erros-=1
+    }
+}
+
+document.onkeydown = (e) => {
+    var letra = e.key.toUpperCase()
+    if(! verificarLetraCorreta(e.key)){
+        if(Secreta.includes(letra)){
+            adicinarLetraCorreta(Secreta.indexOf(letra))
+            for(let i = 0; i < Secreta.length; i++){
+                if(Secreta[i] === letra){
+                    escreverLetraCorreta(i)
+                }
+
+            }
+        }
+    else{
+        if(!verificarLetraCorreta(e.key))
+        return
+        adicionarLetraIncorreta(letra)
+        ecreverLetraIncorreta(letra,erros)
+        
+    }
+}
+
+}
